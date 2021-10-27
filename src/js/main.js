7871ca4ba664ae399493abf1ba437a5a79faebc5
Denis <example@gmail.com>
2021-10-27 12:03:01 +0300

    eighth step, add drag&drop

--- src/js/main.js
+++ src/js/main.js
@@ -9,6 +9,8 @@ import filter from './modules/filter';
 import pictureSize from './modules/pictureSize';
 import accordion from './modules/accordion';
 import burger from './modules/burger';
+import scrolling from './modules/scrolling';
+import drop from './modules/drop';
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -27,5 +29,7 @@ window.addEventListener('DOMContentLoaded', () => {
     pictureSize('.sizes-block');
     accordion('.accordion-heading');
     burger('.burger-menu', '.burger');
+    scrolling('.pageup');
+    drop();
 
 });
